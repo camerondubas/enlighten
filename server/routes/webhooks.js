7700bf9a738e8488f420b94a4f3ae30961e0c2fd
Cameron Dubas <camerondubas@gmail.com>
2016-03-19 15:45:26 -0700

    Web hook route

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -3,6 +3,11 @@
 var express             = require('express');
 var router              = express.Router();
 var MandrillSlackWebhook = require('enlighten-mandrill');
+var MandrillWebhooks = require('../models').MandrillWebhooks;
+
+// console.log('==============================');
+// console.log(MandrillWebhooks.create({mandrillWebhookKey: 'Cameron', slackWebhookEndpoint: 'Dubas'}));
+// console.log('==============================');
 
 router.post('/mandrill', function(req, res, next) {
   var message = new MandrillSlackWebhook(req, process.env.MANDRILL_WEBHOOK_KEY, process.env.MANDRILL_WEBHOOK_ENDPOINT);
@@ -19,6 +24,32 @@ router.post('/mandrill', function(req, res, next) {
     );
 
 //  return next(req.body);
+});
+
+
+router.post('/', function(req, res, next) {
+  console.log('MANDRILL CREATE');
+  console.log(req.body);
+  MandrillWebhooks.create(req.body).then(
+    webhook => next(webhook)
+  ).catch(
+    err => {
+    }
+  );
+//   var message = new MandrillSlackWebhook(req, process.env.MANDRILL_WEBHOOK_KEY, process.env.MANDRILL_WEBHOOK_ENDPOINT);
+
+//   message.sendAllMessages(process.env.SLACK_URL)
+//     .then(
+//       data => {
+//         next(data);
+//         // handle success case;
+//       },
+//       err => {
+//         // handle error case;
+//       }
+//     );
+
+// //  return next(req.body);
 });
 
 module.exports = router;
