28bec5118a873544316c1817d8f39c5c0896b6e4
Cameron Dubas <camerondubas@gmail.com>
2016-03-19 15:45:02 -0700

    Clean up styling, add dependencies

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import './style/style.scss';
 
 import * as angular from 'angular';
 import uiRouter from 'ui-router';
+import ngResource from 'angular-resource';
+
 
 import routes from './app.routes';
 
@@ -12,6 +14,7 @@ import setupPage from './views/setup';
 angular.module('app',
   [
     uiRouter,
+    ngResource,
 
     landingPage,
     setupPage
--- src/views/setup/index.js
+++ src/views/setup/index.js
@@ -3,9 +3,10 @@
 // Local
 import routes from './setup.routes';
 import setupCtrl from './setup.controller';
-
+import Webhooks from './webhooks.resource';
 
 export default angular.module('app.setup', [])
   .config(routes)
   .controller('setupCtrl', setupCtrl)
+  .factory('Webhooks', Webhooks)
   .name;
